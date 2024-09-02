54835e3f17f9261aa9631d48117b5ab7b7276909
unknown <ivan.urbano.n@uni.pe>
2024-09-02 14:25:12 -0500

    Completado

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,13 +1,31 @@
 import heapq
 
 def dijkstra(grafo, inicio):
-    # Inicializamos las distancias con infinito
+    # iniciamos las distancias los nodos como infinito, menos el inicial 
     distancias = {nodo: float('inf') for nodo in grafo}
     distancias[inicio] = 0
-    # Creamos una cola de prioridad para seleccionar la ruta más corta
-    cola_prioridad = [(0, inicio)]
+    
+    # creamos una cola de prioridad que tendra las distancias actuales y el nodo correspondiente
+    cola_prioridad = [(0, inicio)]  # (distancia, nodo)
 
+    while cola_prioridad:
+        # quitamos el nodo con la menor distancia
+        distancia_actual, nodo_actual = heapq.heappop(cola_prioridad)
 
+        # si la distancia quitada es mayor que la registrada entonces ignoramos esta ruta
+        if distancia_actual > distancias[nodo_actual]:
+            continue
 
+        # recorremos los vecinos del nodo 
+        for vecino, peso in grafo[nodo_actual].items():
+            # calculamos la distancia desde el nodo actual hasta el vecino
+            distancia = distancia_actual + peso
 
+            # sii esta distancia es menor que la distancia conocida enotnces la actualizamos
+            if distancia < distancias[vecino]:
+                distancias[vecino] = distancia
+                # añadimos el vecino a la cola de prioridad para otras busquedas
+                heapq.heappush(cola_prioridad, (distancia, vecino))
 
+    # Retornamos el diccionario que contiene la distancia mínima desde el nodo inicial a los otros nodo
+    return distancias
